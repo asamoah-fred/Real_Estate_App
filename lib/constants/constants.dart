a2ab40da6d548b6363466321c85115316d84f5f0
asamoah-fred <asamoahgodfred42@gmail.com>
2021-12-26 01:16:30 +0000

    Edited

---
+++ lib/constants/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+//colors used in this app
+
+const Color white = Colors.white;
+const Color black = Colors.black;
+const Color red = Colors.red;
+const Color pink = Colors.pink;
+const Color darkBlue = Color.fromRGBO(19, 26, 44, 1.0);
+
+//default app padding
+
+const double appPadding = 30.0;
